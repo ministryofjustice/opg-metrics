3922c1817aad3d4fbd36e5fe431ff4f35114429f
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2020-11-06 16:00:14 +0000

    Updated role arn to use shared development (#5)
    * Updated role arn to use shared development
    
    * default to development workspace
    
    Co-authored-by: John Nolan <john.nolan@digital.justice.gov.uk>

--- terraform/service/credentials.tf
+++ terraform/service/credentials.tf
@@ -23,7 +23,7 @@ variable "default_role" {
 provider "aws" {
   region = "eu-west-1"
   assume_role {
-    role_arn     = "arn:aws:iam::311462405659:role/${var.default_role}"
+    role_arn     = "arn:aws:iam::679638075911:role/${var.default_role}"
     session_name = "terraform-session"
   }
 }
