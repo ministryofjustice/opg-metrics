c3b56e96a7c7bc0d860944fcc39ebacedc0bd1c2
John Nolan <john.nolan@digital.justice.gov.uk>
2020-12-18 10:52:36 +0000

    Apply best practices to variables

--- terraform/environment/variables.tf
+++ terraform/environment/variables.tf
@@ -1,7 +1,9 @@
 variable "name" {
   default = "opg-metrics"
+  description = "The name of the service"
 }
 
 variable "flink_name" {
   default = "opg-metrics-flink"
+  description = "Name used for Kinesis Analytics Application and its resources."
 }
