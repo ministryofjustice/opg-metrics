37db586e88077a7dd020c07d2551b2e7bdc089b9
John Nolan <john.nolan@digital.justice.gov.uk>
2020-12-29 10:22:45 +0000

    Moved API Gateway to seperate swagger document

--- terraform/environment/api-gateway.tf
+++ terraform/environment/api-gateway.tf
@@ -37,102 +37,18 @@ resource "aws_api_gateway_rest_api" "kinesis_stream_api_gateway" {
     types = ["REGIONAL"]
   }
 
-  body = <<EOF
-  {
-  "swagger": "2.0",
-  "info": {
-    "title": "kinesis-stream"
-  },
-  "basePath": "/kinesis",
-  "schemes": [
-    "https"
-  ],
-  "paths": {
-    "/streams": {
-      "get": {
-        "consumes": [
-          "application/json"
-        ],
-        "produces": [
-          "application/json"
-        ],
-        "responses": {
-          "200": {
-            "description": "200 response",
-            "schema": {
-              "$ref": "#/definitions/Empty"
-            }
-          }
-        },
-        "x-amazon-apigateway-integration": {
-          "type": "aws",
-          "credentials": "${aws_iam_role.kinesis_apigateway.arn}",
-          "uri": "arn:aws:apigateway:${data.aws_region.current.name}:kinesis:action/ListStreams",
-          "responses": {
-            "default": {
-              "statusCode": "200"
-            }
-          },
-          "requestTemplates": {
-            "application/json": "{}"
-          },
-          "passthroughBehavior": "when_no_templates",
-          "httpMethod": "POST"
-        }
-      }
-    },
-    "/streams/${var.name}/records": {
-      "put": {
-        "consumes": [
-          "application/json"
-        ],
-        "produces": [
-          "application/json"
-        ],
-        "responses": {
-          "200": {
-            "description": "200 response",
-            "schema": {
-              "$ref": "#/definitions/Empty"
-            }
-          }
-        },
-        "x-amazon-apigateway-integration": {
-          "type": "aws",
-          "credentials": "${aws_iam_role.kinesis_apigateway.arn}",
-          "uri": "arn:aws:apigateway:${data.aws_region.current.name}:kinesis:action/PutRecords",
-          "responses": {
-            "default": {
-              "statusCode": "200"
-            }
-          },
-          "requestTemplates": {
-            "application/json": "{\n    \"StreamName\": \"${var.name}\",\n    \"Records\": [\n       #foreach($elem in $input.path('$.records'))\n          {\n            \"Data\": \"$util.base64Encode($elem.data)\",\n            \"PartitionKey\": \"$elem.partition-key\"\n          }#if($foreach.hasNext),#end\n        #end\n    ]\n}"
-          },
-          "passthroughBehavior": "when_no_templates",
-          "httpMethod": "POST"
-        }
-      }
-    }
-  },
-  "definitions": {
-    "Empty": {
-      "type": "object",
-      "title": "Empty Schema"
-    },
-    "PutRecord": {
-      "type": "object",
-      "properties": {
-        "Data": {
-          "type": "string"
-        },
-        "PartitionKey": {
-          "type": "string"
-        }
-      },
-      "title": "PutRecord proxy single-record payload"
-    }
+  body = data.template_file._.rendered
+}
+
+locals {
+  openapispec = file("./swagger.json")
+  swagger_template_vars = {
+    region        = data.aws_region.current.name
+    name          = var.name
+    allowed_roles = aws_iam_role.kinesis_apigateway.arn
   }
 }
-EOF
+data "template_file" "_" {
+  template = local.openapispec
+  vars     = local.swagger_template_vars
 }
