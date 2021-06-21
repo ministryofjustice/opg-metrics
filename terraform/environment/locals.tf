d864701ac256d1a6ef3d239a50a18c0a066bc6fe
John Nolan <john.nolan@digital.justice.gov.uk>
2021-06-21 08:17:26 +0100

    Fix typo

--- terraform/environment/locals.tf
+++ terraform/environment/locals.tf
@@ -2,7 +2,7 @@ locals {
   account_name = lookup(var.account_mapping, terraform.workspace, "development")
   environment  = lower(terraform.workspace)
 
-  dns_namespace_acc = local.environment == "production" ? "" : "${local.account_name}."
+  dns_namespace_env = local.environment == "production" ? "" : "${local.account_name}."
 
   mandatory_moj_tags = {
     business-unit    = "OPG"
