01f5374775ffa83356e6446b7513153f242783c8
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2022-02-08 10:40:35 +0000

    UML-2084 - Create api key for costs (#41)
    * create an api key for costs
    
    * lockfile
    
    * allow management to decrypt

--- terraform/environment/api-keys.tf
+++ terraform/environment/api-keys.tf
@@ -21,3 +21,15 @@ module "trusted_service_access_use_a_lasting_power_of_attorney_development" {
     "arn:aws:iam::367815980639:root"
   ]
 }
+
+module "trusted_service_access_costs_to_metrics_development" {
+  trusted_service_name               = "costs-to-metrics-development"
+  source                             = "../modules/trusted_service_access"
+  aws_api_gateway_rest_api           = aws_api_gateway_rest_api.kinesis_stream_api_gateway.id
+  aws_api_gateway_stage              = aws_api_gateway_stage.kinesis_stream_api_gateway.stage_name
+  secret_recovery_window_in_days     = 0
+  secretsmanager_api_keys_kms_key_id = aws_kms_key.api_key.key_id
+  identifiers_arns = [
+    "arn:aws:iam::311462405659:root"
+  ]
+}
--- terraform/environment/kms.tf
+++ terraform/environment/kms.tf
@@ -39,7 +39,8 @@ data "aws_iam_policy_document" "api_key_kms" {
     principals {
       type = "AWS"
       identifiers = [
-        "arn:aws:iam::367815980639:root"
+        "arn:aws:iam::367815980639:root",
+        "arn:aws:iam::311462405659:root"
       ]
     }
   }
