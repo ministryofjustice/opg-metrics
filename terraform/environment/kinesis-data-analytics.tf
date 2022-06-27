380f538abed6bfd2fa6f91d7d015289ea9b93695
John Nolan <john.nolan@digital.justice.gov.uk>
2022-06-27 14:48:13 +0100

    tfsec: S3 Access block should block public ACL

--- terraform/environment/kinesis-data-analytics.tf
+++ terraform/environment/kinesis-data-analytics.tf
@@ -1,5 +1,11 @@
+#tfsec:ignore:aws-s3-enable-versioning
 resource "aws_s3_bucket" "flink" {
   bucket = var.flink_name
+}
+
+resource "aws_s3_bucket_public_access_block" "flink" {
+  bucket            = aws_s3_bucket.flink.id
+  block_public_acls = true
 }
 
 resource "aws_s3_bucket_object" "flink" {
