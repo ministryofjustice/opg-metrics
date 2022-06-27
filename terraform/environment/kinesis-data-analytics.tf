90ef610e756200f30eba608129dfcf91e22ab6bd
John Nolan <john.nolan@digital.justice.gov.uk>
2022-06-27 14:51:16 +0100

    tfsec: S3 Access Block should Ignore Public Acl

--- terraform/environment/kinesis-data-analytics.tf
+++ terraform/environment/kinesis-data-analytics.tf
@@ -7,6 +7,7 @@ resource "aws_s3_bucket_public_access_block" "flink" {
   bucket              = aws_s3_bucket.flink.id
   block_public_acls   = true
   block_public_policy = true
+  ignore_public_acls  = true
 }
 
 resource "aws_s3_bucket_object" "flink" {
--- terraform/environment/s3.tf
+++ terraform/environment/s3.tf
@@ -7,4 +7,5 @@ resource "aws_s3_bucket_public_access_block" "kinesis_data_analytics" {
   bucket              = aws_s3_bucket.kinesis_data_analytics.id
   block_public_acls   = true
   block_public_policy = true
+  ignore_public_acls  = true
 }
