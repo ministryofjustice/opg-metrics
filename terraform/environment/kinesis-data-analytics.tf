75355a7df0b85dfa998067ae749e708498ad69a0
John Nolan <john.nolan@digital.justice.gov.uk>
2022-06-27 14:52:15 +0100

    tfsec: S3 Access block should restrict public bucket to limit access

--- terraform/environment/kinesis-data-analytics.tf
+++ terraform/environment/kinesis-data-analytics.tf
@@ -8,10 +8,11 @@ variable "timestream_artifact_name" {
 }
 
 resource "aws_s3_bucket_public_access_block" "flink" {
-  bucket              = aws_s3_bucket.flink.id
-  block_public_acls   = true
-  block_public_policy = true
-  ignore_public_acls  = true
+  bucket                  = aws_s3_bucket.flink.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 resource "aws_s3_bucket_object" "flink" {
