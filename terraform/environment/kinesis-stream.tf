1c22654454532339409af7cb8aa7c2a2a283693d
John Nolan <john.nolan@digital.justice.gov.uk>
2022-07-21 09:24:20 +0100

    Roll back of KMS key for tfesc warnings (#57)
    * Roll back of KMS key for tfesc warnings

--- terraform/environment/kinesis-stream.tf
+++ terraform/environment/kinesis-stream.tf
@@ -2,6 +2,4 @@ resource "aws_kinesis_stream" "metrics_input" {
   name             = var.name
   shard_count      = 1
   retention_period = 24
-  encryption_type  = "KMS"
-  kms_key_id       = aws_kms_key.kinesis_metrics_input.id
 }
