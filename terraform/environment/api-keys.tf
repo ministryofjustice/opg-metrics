9088fc8638ae3f14e59a5182008f4edc958360ca
Greg Tyler <gregtyler@users.noreply.github.com>
2022-08-03 12:45:44 +0100

    Create service access for Sirius dev account
    So we can start sharing case status metrics
    
    VEGA-1377

--- terraform/environment/api-keys.tf
+++ terraform/environment/api-keys.tf
@@ -33,3 +33,15 @@ module "trusted_service_access_costs_to_metrics_development" {
     "arn:aws:iam::311462405659:root"
   ]
 }
+  
+module "trusted_service_access_sirius_development" {
+  trusted_service_name               = "sirius-development"
+  source                             = "../modules/trusted_service_access"
+  aws_api_gateway_rest_api           = aws_api_gateway_rest_api.kinesis_stream_api_gateway.id
+  aws_api_gateway_stage              = aws_api_gateway_stage.kinesis_stream_api_gateway.stage_name
+  secret_recovery_window_in_days     = 0
+  secretsmanager_api_keys_kms_key_id = aws_kms_key.api_key.key_id
+  identifiers_arns = [
+    "arn:aws:iam::288342028542:root"
+  ]
+}
