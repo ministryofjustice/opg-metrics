199fd1bb1ce4e427369b670a09463fe4967d511f
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2022-08-09 12:41:16 +0100

    Fix typo in role ARNs
    They were missing the `role/` prefix

--- terraform/environment/kms.tf
+++ terraform/environment/kms.tf
@@ -42,10 +42,10 @@ data "aws_iam_policy_document" "api_key_kms" {
         "arn:aws:iam::367815980639:root",
         "arn:aws:iam::311462405659:root",
         "arn:aws:iam::288342028542:root",
-        "arn:aws:iam::492687888235:api-ecs-preproduction-20200204165900331400000008",
-        "arn:aws:iam::492687888235:api-ecs-preproduction-2021040714392497160000000a",
-        "arn:aws:iam::649098267436:api-ecs-production-20190802114727697300000001",
-        "arn:aws:iam::649098267436:api-ecs-production-2021080908515105620000000a"
+        "arn:aws:iam::492687888235:role/api-ecs-preproduction-20200204165900331400000008",
+        "arn:aws:iam::492687888235:role/api-ecs-preproduction-2021040714392497160000000a",
+        "arn:aws:iam::649098267436:role/api-ecs-production-20190802114727697300000001",
+        "arn:aws:iam::649098267436:role/api-ecs-production-2021080908515105620000000a"
       ]
     }
   }
