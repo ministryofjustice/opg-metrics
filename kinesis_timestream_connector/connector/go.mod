8533d6aed55a0f2cad20aaec1a2dc96f2356efe5
lukejolliffe <luke.jolliffe@digital.justice.gov.uk>
2022-12-20 13:49:00 +0000

    add basic lambda for connecting kinesis to timestream

---
+++ kinesis_timestream_connector/connector/go.mod
@@ -0,0 +1,31 @@
+module opg-metrics
+
+go 1.18
+
+require (
+	github.com/aws/aws-sdk-go-v2/service/kinesis v1.15.25
+	github.com/ministryofjustice/opg-go-common v0.0.0-20220816144329-763497f29f90
+)
+
+require (
+	github.com/aws/aws-sdk-go-v2/credentials v1.13.4 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.12.20 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.3.27 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/endpoint-discovery v1.7.20 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.9.20 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.11.26 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.13.9 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.17.6 // indirect
+)
+
+require (
+	github.com/aws/aws-lambda-go v1.35.0
+	github.com/aws/aws-sdk-go-v2 v1.17.2 // indirect
+	github.com/aws/aws-sdk-go-v2/aws/protocol/eventstream v1.4.10 // indirect
+	github.com/aws/aws-sdk-go-v2/config v1.18.4
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.1.26 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.4.20 // indirect
+	github.com/aws/aws-sdk-go-v2/service/timestreamwrite v1.14.7
+	github.com/aws/smithy-go v1.13.5 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+)
---
+++ kinesis_timestream_connector/connector/main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/timestreamwrite"
+	"github.com/aws/aws-sdk-go-v2/service/timestreamwrite/types"
+)
+
+type handler struct {
+	c *timestreamwrite.Client
+}
+
+func main() {
+	cfg, err := config.LoadDefaultConfig(context.Background())
+	if err != nil {
+		log.Panic("Error loading default config")
+	}
+	c := timestreamwrite.NewFromConfig(cfg)
+
+	lambdaHandler := &handler{c}
+	lambda.Start(lambdaHandler.Handle)
+}
+
+func (h handler) Handle(ctx context.Context, kinesisEvent events.KinesisEvent) {
+	kinesisRecords := make([]map[string]string, len(kinesisEvent.Records))
+
+	for i, record := range kinesisEvent.Records {
+		x := map[string]string{}
+		err := json.Unmarshal(record.Kinesis.Data, &x)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		kinesisRecords[i] = x
+	}
+
+	records := kinesisRecordsToTimestreamRecords(kinesisRecords)
+	h.c.WriteRecords(context.Background(), &timestreamwrite.WriteRecordsInput{
+		DatabaseName: aws.String("opg-metrics"),
+		Records:      records,
+		TableName:    aws.String("opg-metrics"),
+	})
+}
+
+func kinesisRecordsToTimestreamRecords(kinesisRecords []map[string]string) []types.Record {
+	records := make([]types.Record, 0)
+	for _, kinesisRecord := range kinesisRecords {
+		dimensions := make([]types.Dimension, 0)
+		for key, value := range kinesisRecord {
+			if !isMandatoryData(key) {
+				dimensions = append(dimensions, types.Dimension{Name: &key, Value: &value})
+			}
+		}
+		records = append(records, types.Record{
+			Dimensions:       dimensions,
+			MeasureName:      aws.String(kinesisRecord["MeasureName"]),
+			MeasureValue:     aws.String(kinesisRecord["MeasureValue"]),
+			MeasureValueType: types.MeasureValueTypeDouble,
+			Time:             aws.String(kinesisRecord["Time"]),
+			TimeUnit:         types.TimeUnitMilliseconds,
+		})
+
+	}
+	return records
+}
+
+func isMandatoryData(key string) bool {
+	switch key {
+	case
+		"MeasureName",
+		"MeasureValue",
+		"Time":
+		return true
+	}
+	return false
+}
