8f5b6b6a03bf532e3387447a90d05586ba97a6c9
John Nolan <john.nolan@digital.justice.gov.uk>
2023-01-09 17:34:04 +0000

    Add basic timestream tf

---
+++ terraform/environment/timestream.tf
@@ -0,0 +1,22 @@
+resource "aws_timestreamwrite_database" "timestream_database" {
+  database_name = var.name
+  kms_key_id    = aws_kms_key.example.arn
+
+  tags = {
+    Name = "value"
+  }
+}
+
+resource "aws_timestreamwrite_table" "timestream_database" {
+  database_name = aws_timestreamwrite_database.timestream_database.database_name
+  table_name    = var.name
+
+  retention_properties {
+    magnetic_store_retention_period_in_days = 30
+    memory_store_retention_period_in_hours  = 8
+  }
+
+  tags = {
+    Name = local.mandatory_moj_tags
+  }
+}
