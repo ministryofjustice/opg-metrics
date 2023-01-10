513442483ab4ac28d68a96ec33a5f6c829934849
John Nolan <john.nolan@digital.justice.gov.uk>
2023-01-10 09:29:27 +0000

    Remove uneeded tags as set globally

--- terraform/environment/timestream.tf
+++ terraform/environment/timestream.tf
@@ -1,9 +1,5 @@
 resource "aws_timestreamwrite_database" "timestream_database" {
   database_name = var.name
-
-  tags = {
-    Name = "value"
-  }
 }
 
 resource "aws_timestreamwrite_table" "timestream_database" {
@@ -13,9 +9,5 @@ resource "aws_timestreamwrite_table" "timestream_database" {
   retention_properties {
     magnetic_store_retention_period_in_days = 30
     memory_store_retention_period_in_hours  = 8
-  }
-
-  tags = {
-    Name = local.mandatory_moj_tags
   }
 }
