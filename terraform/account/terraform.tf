39d16171fc55563305b747c8dc11c0cfd31fdd1e
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2025-01-02 10:05:39 +0000

    fix validation issues:

--- terraform/account/terraform.tf
+++ terraform/account/terraform.tf
@@ -1,10 +1,12 @@
 terraform {
   backend "s3" {
-    bucket         = "opg.terraform.state"
-    key            = "opg-metrics-shared/terraform.tfstate"
-    encrypt        = true
-    region         = "eu-west-1"
-    role_arn       = "arn:aws:iam::311462405659:role/opg-metrics-ci"
+    bucket  = "opg.terraform.state"
+    key     = "opg-metrics-shared/terraform.tfstate"
+    encrypt = true
+    region  = "eu-west-1"
+    assume_role = {
+      role_arn = "arn:aws:iam::311462405659:role/opg-metrics-ci"
+    }
     dynamodb_table = "remote_lock"
   }
   required_providers {
