8f028cc1507809fc1e80d905e37073fc60055b85
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2025-04-24 21:14:47 +0100

    refactor to support production deployments in the future

---
+++ terraform/account/variables.tf
@@ -0,0 +1,25 @@
+variable "accounts" {
+  type = map(
+    object({
+      account_id = string
+    })
+  )
+}
+
+locals {
+  account_name      = lower(replace(terraform.workspace, "_", "-"))
+  account           = contains(keys(var.accounts), local.account_name) ? var.accounts[local.account_name] : var.accounts["default"]
+  dns_namespace_env = local.account_name == "production" ? "" : "${local.account_name}."
+
+  mandatory_moj_tags = {
+    business-unit    = "OPG"
+    application      = "opg-metrics-shared"
+    environment-name = local.account_name
+    owner            = "OPGOPS opgteam+opgmetrics@digital.justice.gov.uk"
+    is-production    = "false"
+    runbook          = "https://github.com/ministryofjustice/opg-metrics"
+    source-code      = "https://github.com/ministryofjustice/opg-metrics"
+  }
+
+  tags = local.mandatory_moj_tags
+}
