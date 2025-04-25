4a027f39ea52577a6dd097159146ed4bf4adcead
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2025-04-25 10:00:41 +0100

    MLPAB-2967 - fix rotate api keys job in org infra (#151)
    * manage terraform version with scripts
    
    * manage terraform version with scripts
    
    * use opg actions for setting terraform version on GA
    
    * replace template file provider with templatefile function
    
    * manage terraform version with script
    
    * remove tfswitch file
    
    * fix versioning of archive provider
    
    * fix variable types
    
    * use data source to address linting issue of data source not being referenced
    
    * fix type of name variable
    
    * remove stage-name from deployment as this is deprecated in favour of the aws_api_gateway_stage resource
    
    * upgrade aws creds action
    
    * use templatefile function directly
    
    * refactor to allow for prod deployments
    
    * use correct account id
    
    * add shared prod account name
    
    * add shared prod account name
    
    * reduce batch size for event sourcing from default 100
    
    * set event souring batch size to kinesis default
    
    * tidy up json
    
    * remove unused comments
    
    * refactor to support production deployments in the future
    
    * use environment_name
    
    * replace deprecated attribute
    
    * use tfvars to select correct account
    
    * set specific aws provider version
    
    * run tflint on the pipeline
    
    * replace outdated codeql action with maintained version
    
    * use previous schedule
    
    * update upload sarif action
    
    * upgrade actions

--- terraform/account/variables.tf
+++ terraform/account/variables.tf
@@ -1,8 +1,19 @@
+variable "accounts" {
+  type = map(
+    object({
+      account_id = string
+    })
+  )
+}
+
 locals {
+  account_name = lower(replace(terraform.workspace, "_", "-"))
+  account      = contains(keys(var.accounts), local.account_name) ? var.accounts[local.account_name] : var.accounts["default"]
+
   mandatory_moj_tags = {
     business-unit    = "OPG"
     application      = "opg-metrics-shared"
-    environment-name = terraform.workspace
+    environment-name = local.account_name
     owner            = "OPGOPS opgteam+opgmetrics@digital.justice.gov.uk"
     is-production    = "false"
     runbook          = "https://github.com/ministryofjustice/opg-metrics"
