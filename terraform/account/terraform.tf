fdf8bcdb62686dec3fd02421caa295013d85bc7a
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2025-04-25 14:43:26 +0100

    use new state role and switch to state lockfile (#152)

--- terraform/account/terraform.tf
+++ terraform/account/terraform.tf
@@ -5,9 +5,9 @@ terraform {
     encrypt = true
     region  = "eu-west-1"
     assume_role = {
-      role_arn = "arn:aws:iam::311462405659:role/opg-metrics-ci"
+      role_arn = "arn:aws:iam::311462405659:role/opg-metrics-state-access"
     }
-    dynamodb_table = "remote_lock"
+    use_lockfile = true
   }
 }
 
