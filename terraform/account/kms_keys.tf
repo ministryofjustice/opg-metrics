d234a8a4057621ca77f810082666b0c84a61f6b9
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2025-04-29 13:19:04 +0100

    rename key as it lives in a shared account

--- terraform/account/kms_keys.tf
+++ terraform/account/kms_keys.tf
@@ -3,7 +3,7 @@ module "cloudwatch_log_group_kms_key" {
   administrator_roles = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/breakglass"]
   alias               = "${data.aws_default_tags.current.tags.application}-cloudwatch-log-groups-${local.account_name}"
   decryption_roles    = [local.account_name == "production" ? "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/breakglass" : "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/operator"]
-  description         = "KMS Key for Cloudwatch log group encryption ${local.account_name}"
+  description         = "KMS key for opg-metrics cloudwatch log group encryption ${local.account_name}"
   encryption_roles    = []
   usage_services = [
     "logs.eu-west-1.amazonaws.com",
