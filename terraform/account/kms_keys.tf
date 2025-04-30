7dcd217cfcab8056e10f7d3effe2fa8fd4a8fb4b
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2025-04-30 15:41:27 +0100

    Revert API gateway log group encryption (#167)
    * allow ci as admin for key
    
    * remove encryption from log group
    
    * sidestep lint error

--- terraform/account/kms_keys.tf
+++ terraform/account/kms_keys.tf
@@ -1,10 +1,13 @@
 module "cloudwatch_log_group_kms_key" {
-  source              = "../modules/kms_key"
-  administrator_roles = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/breakglass"]
-  alias               = "${data.aws_default_tags.current.tags.application}-cloudwatch-log-groups-${local.account_name}"
-  decryption_roles    = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/operator"]
-  description         = "KMS key for opg-metrics cloudwatch log group encryption ${local.account_name}"
-  encryption_roles    = []
+  source = "../modules/kms_key"
+  administrator_roles = [
+    "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/breakglass",
+    "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/opg-metrics-ci"
+  ]
+  alias            = "${data.aws_default_tags.current.tags.application}-cloudwatch-log-groups-${local.account_name}"
+  decryption_roles = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/operator"]
+  description      = "KMS key for opg-metrics cloudwatch log group encryption ${local.account_name}"
+  encryption_roles = []
   usage_services = [
     "logs.eu-west-1.amazonaws.com",
     "logs.eu-west-2.amazonaws.com",
