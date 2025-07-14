0725b47b5899805d24a390ea3e77b0a54a70ad47
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2025-07-14 13:59:03 +0100

    provide id for key not for alias

--- terraform/environment/kinesis_stream.tf
+++ terraform/environment/kinesis_stream.tf
@@ -3,5 +3,5 @@ resource "aws_kinesis_stream" "metrics_input" {
   shard_count      = 1
   retention_period = 24
   encryption_type  = "KMS"
-  kms_key_id       = data.aws_kms_alias.kinesis_encryption.id
+  kms_key_id       = data.aws_kms_alias.kinesis_encryption.target_key_id
 }
