d9902f264f0c094f279deaf9f1278a84ea14cd66
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2025-07-14 13:18:38 +0100

    add data sources and rename files

---
+++ terraform/environment/data_sources.tf
@@ -0,0 +1,9 @@
+data "aws_region" "current" {}
+
+data "aws_caller_identity" "current" {}
+
+# data "aws_default_tags" "current" {}
+
+data "aws_kms_alias" "cloudwatch_application_logs_encryption" {
+  name = "alias/${data.aws_default_tags.current.tags.application}-cloudwatch-log-groups-${local.environment.account_name}"
+}
